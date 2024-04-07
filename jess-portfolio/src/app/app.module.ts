929a073b90d72aacbf47a2f7aa4bb30d28e60329
Jessica James <jessicajames@jessicas-mbp.lan>
2024-04-07 16:22:49 -0600

    Initial portfolio Angular app push

---
+++ jess-portfolio/src/app/app.module.ts
@@ -0,0 +1,20 @@
+// app.module.ts
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule  // This should come after BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
